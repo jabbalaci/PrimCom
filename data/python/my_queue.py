7826c250128a5701343de0d010bd6ce03ea27827
Jabba Laci <jabba.laci@gmail.com>
2013-09-04 10:29:02 +0200

    stack and queue updated

--- data/python/my_queue.py
+++ data/python/my_queue.py
@@ -7,3 +7,16 @@ for i in range(5):
 
 while not q.empty():
     print q.get()    # 0 1 2 3 4
+
+## or: ##################
+
+>>> from collections import deque
+>>>
+>>> q = deque()
+>>> q.append(1); q.append(3); q.append(5)    # q = deque([1,3,5])
+>>> q
+deque([1, 3, 5])
+>>> q.popleft()
+1
+>>> q
+deque([3, 5])
--- data/python/my_stack.py
+++ data/python/my_stack.py
@@ -7,3 +7,18 @@ for i in range(5):
 
 while not q.empty():
     print q.get()    # 4 3 2 1 0
+
+## or: ##################
+
+>>> li
+[1, 4, 6]
+>>> li.append(8)
+>>> li
+[1, 4, 6, 8]
+>>> while li:
+...     print li.pop()
+... 
+8
+6
+4
+1
