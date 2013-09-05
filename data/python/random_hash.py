d74238c498d72fd0ffe959409724e2a63e07457a
Jabba Laci <jabba.laci@gmail.com>
2013-09-05 13:19:56 +0200

    indent check

--- data/python/random_hash.py
+++ data/python/random_hash.py
@@ -1,6 +1,5 @@
 import random
 
-
 def my_hash(bits=96):
     assert bits % 8 == 0
     required_length = bits / 8 * 2
@@ -12,3 +11,16 @@ def my_hash(bits=96):
 
 # print my_hash()
 # 9c14dac2a6f1d65929095295    # just a sample; it's a string
+
+## or: ##################
+
+# You can also use simpleflake. By default it generates 64 bit
+# unique IDs. The generated ID consists of 2 parts: a) timestamp,
+# b) random number. It has the advantage that the generated IDs
+# arrive in ascending order.
+# see http://engineering.custommade.com/simpleflake-distributed-id-generation-for-the-lazy/
+
+$ pip install simpleflake
+>>> import simpleflake
+>>> simpleflake.simpleflake()
+3620361890155888216L    # just a sample
