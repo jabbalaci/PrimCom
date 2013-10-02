9cd62c301d4f179f38b0e5d075f93bfb4e3323a1
Jabba Laci <jabba.laci@gmail.com>
2013-10-02 13:16:34 +0200

    content added

---
+++ data/python/my_md5.py
@@ -0,0 +1,20 @@
+import hashlib
+
+
+def string_to_md5(content):
+    """Calculate the md5 hash of a string.
+    
+    This 'string' can be the binary content of a file too."""
+    return hashlib.md5(content).hexdigest()
+
+
+def file_to_md5(filename):
+    """Calculate the md5 hash of a file. Memory-friendly solution,
+    it reads the file piece by piece.
+    
+    http://stackoverflow.com/questions/1131220/get-md5-hash-of-a-files-without-open-it-in-python"""
+    md5 = hashlib.md5()
+    with open(filename,'rb') as f:
+        for chunk in iter(lambda: f.read(8192), ''):
+            md5.update(chunk)
+    return md5.hexdigest()
