e73abd0a0d9a8adae36e09783c4cdd0b0eb7bd96
Jabba Laci <jabba.laci@gmail.com>
2013-11-28 14:16:41 +0100

    shortener

--- modules/urlshortener.py
+++ modules/urlshortener.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python
+
 import os
 import sys
 sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
@@ -5,7 +7,6 @@ sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
 import requests
 from lib.clipboard import text_to_clipboards
 import json
-
 
 def shorten_url(long_url):
     try:
@@ -18,3 +19,19 @@ def shorten_url(long_url):
         text_to_clipboards(short)
     except:
         print "Hmm, strange..."
+
+
+#def shorten_url(long_url):
+#    try:
+#        url = "http://gg.gg/?action=create&url=" + long_url
+#        r = requests.get(url)
+#        short = r.text
+#        print short
+#        text_to_clipboards(short)
+#    except:
+#        print "Hmm, strange..."
+
+
+if __name__ == "__main__":
+    url = raw_input("Long URL: ")
+    shorten_url(url)
