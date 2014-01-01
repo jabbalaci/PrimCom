f9102159b910603de3e6a5f31508a0e4a818d9de
Jabba Laci <jabba.laci@gmail.com>
2014-01-01 17:13:58 +0100

    import from parent using the imp module

--- data/python/import-from-parent-dir.py
+++ data/python/import-from-parent-dir.py
@@ -2,7 +2,7 @@
 import os
 import sys
 
-# import from parent directory, i.e. 
+# import from parent directory, i.e.
 # add the path of the parent directory to sys.path
 if __name__ == "__main__":
     sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
@@ -12,3 +12,17 @@ if __name__ == "__main__":
 import sys
 # to be able to import something from the lib (if normal import didn't work)
 sys.path.insert(0, "./lib/")
+
+# ~~~~~~~~~~~~~~~~~~~~
+
+def load_src(rel_path, name=None):
+    import os, imp
+    if name is None:
+        name = os.path.splitext(os.path.split(rel_path)[1])[0]
+    return imp.load_source(name, os.path.join(os.path.dirname(__file__), rel_path))
+
+eh = load_src("../helper/eh.py")    # equivalent to "import eh"
+gotcha = load_src("./gotcha.py")    # equivalent to "import gotcha"
+
+from gotcha import semmi
+print eh.SOMETHING
