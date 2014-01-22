1bf583af18d4340e5b3b853bb6bb66ae714c7254
Jabba Laci <jabba.laci@gmail.com>
2014-01-22 15:07:08 +0100

    newline

--- data/shell/xsel.sh
+++ data/shell/xsel.sh
@@ -1,7 +1,9 @@
 # copy string to "primary" clipboard
 echo "hello" | xsel -pi
+
 # copy string to "clipboard" clipboard
 echo "hello" | xsel -bi
+
 # print the content of the clipboards
 # primary and clipboard, respectively
 xsel -op
