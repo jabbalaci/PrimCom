559c42f61a22ebc9563b454b0a53be164cc9f58e
Jabba Laci <jabba.laci@gmail.com>
2014-03-04 14:44:39 +0100

    pip: update local packages

--- data/python/update-pip.py
+++ data/python/update-pip.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+"""
+Global version. It updates packages that were installed with "sudo".
+"""
 import os
 import pip
 
@@ -11,3 +14,10 @@ for dist_name in sorted(dists, key=lambda s: s.lower()):
     cmd = "sudo pip install {0} -U".format(dist_name)
     print '#', cmd
     os.system(cmd)
+
+########################################
+
+"""
+local version, can be used in a virtualenv for instance
+"""
+pip freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs pip install -U
