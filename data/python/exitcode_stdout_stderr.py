ce10f9461f10bf030462152bd8ce78afd2361f03
Jabba Laci <jabba.laci@gmail.com>
2014-03-13 16:03:32 +0100

    new content

---
+++ data/python/exitcode_stdout_stderr.py
@@ -0,0 +1,15 @@
+import shlex
+from subprocess import Popen, PIPE
+
+
+def get_exitcode_stdout_stderr(cmd):
+    """
+    Execute the external command and get its exitcode, stdout and stderr.
+    """
+    args = shlex.split(cmd)
+
+    proc = Popen(args, stdout=PIPE, stderr=PIPE)
+    out, err = proc.communicate()
+    exitcode = proc.returncode
+    #
+    return exitcode, out, err
